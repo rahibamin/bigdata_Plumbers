4afd00df97aa5f8ff7941453ed3fbaf391e39159
rpm-overclock <Jaime.Corchado@EnhanceIt.us>
2020-07-16 16:23:13 -0400

    Mapping - From Hadoop MapReduce ported.

--- task_006_wordcount-again/WordCount.java
+++ task_006_wordcount-again/WordCount.java
@@ -1,77 +1,30 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package org.apache.hadoop.mapred;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.io.FileInputStream;
+import java.util.HashSet;
+import java.math.BigInteger;
+import java.nio.charset.Charset;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
-
-/**
- * This is an example Hadoop Map/Reduce application.
- * It reads the text input files, breaks each line into words
- * and counts them. The output is a locally sorted list of words and the 
- * count of how often they occurred.
- *
- * To run: bin/hadoop jar build/hadoop-examples.jar wordcount
- *            [-m <i>maps</i>] [-r <i>reduces</i>] <i>in-dir</i> <i>out-dir</i> 
- */
-public class WordCount extends Configured implements Tool {
+public class WordCount {
   
   /**
    * Counts the words in each line.
    * For each line of input, break the line into words and emit them as
    * (<b>word</b>, <b>1</b>).
    */
-  public static class MapClass extends MapReduceBase
-    implements Mapper<LongWritable, Text, Text, IntWritable> {
-    
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
-    
-    public void map(LongWritable key, Text value, 
-                    OutputCollector<Text, IntWritable> output, 
-                    Reporter reporter) throws IOException {
+  public static class MapClass {
+    public java.util.HashSet<String,BigInteger> map(String key, java.io.InputStream value) {
+      java.util.HashSet<String,BigInteger> output = new java.util.HashSet<String,BigInteger>();
       String line = value.toString();
       StringTokenizer itr = new StringTokenizer(line);
       while (itr.hasMoreTokens()) {
-        word.set(itr.nextToken());
-        output.collect(word, one);
+	String word = itr.nextToken();
+	output.putIfAbsent(word,java.math.BigInteger.ZERO);
+        output.put(word,java.math.BigInteger.ONE);
       }
     }
   }
@@ -152,6 +105,7 @@ public class WordCount extends Configured implements Tool {
   
   
   public static void main(String[] args) throws Exception {
+    private final static java.io.FileInputStream one = new java.io.FileInputStream("./input.txt");
     int res = ToolRunner.run(new Configuration(), new WordCount(), args);
     System.exit(res);
   }
