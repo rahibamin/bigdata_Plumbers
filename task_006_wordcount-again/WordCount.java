4344e82fd4bb16458c524ce025bfc84a42675639
rpm-overclock <Jaime.Corchado@EnhanceIt.us>
2020-07-16 09:38:40 -0400

    Adding hashmap in Java

--- task_006_wordcount-again/WordCount.java
+++ task_006_wordcount-again/WordCount.java
@@ -1,8 +1,11 @@
 
+import java.util.HashMap;
+import java.lang.System;
+
 public class WordCount {
+	public java.util.HashMap<String,String> words = new java.util.HashMap<String,String>();
 	public static void main(String[] args) {
-		for (String x : args){
-			System.out.println(x);
-		}
+		WordCount parsing = new WordCount();
+		parsing.words.putIfAbsent();
 	}
 }
